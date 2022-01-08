b813af6a48fc216f08b089ed4409ae6316b52b78
theblindbandet <theblindbandet@users.noreply.github.com>
2022-01-08 16:14:02 -0500

    Add Loggable compressor

---
+++ src/main/java/frc/robot/logging/LoggableCompressor.java
@@ -0,0 +1,22 @@
+package frc.robot.logging;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+public class LoggableCompressor extends Compressor implements Loggable {
+    public LoggableCompressor(int module, PneumaticsModuleType moduleType) {
+        super(module, moduleType);
+    }
+    public LoggableCompressor(PneumaticsModuleType moduleType) {
+        super(moduleType);
+    }
+
+    @Override
+    public void setupLogging(Logger logger) {
+        logger.addAttribute("pressure");
+    }
+    @Override
+    public void log(Logger logger) {
+        logger.log("pressure", this.getPressure());
+    }
+}
