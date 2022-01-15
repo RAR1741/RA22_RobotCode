065285cdd1f9c0d22144e43630d4604928848f9b
theblindbandet <theblindbandet@users.noreply.github.com>
2022-01-15 13:25:32 -0500

    Add basic shooter class I don't actually really know what the actual specs of the shooter are. This was mostly made as a tutorial to git for the rest of the SPEC team.

---
+++ src/main/java/frc/robot/Shooter.java
@@ -0,0 +1,39 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import frc.robot.logging.Loggable;
+import frc.robot.logging.Logger;
+
+public class Shooter implements Loggable {
+    TalonFX shooterMotor;
+    public Shooter(TalonFX s) {
+        this.shooterMotor = s;
+
+        this.shooterMotor.setNeutralMode(NeutralMode.Coast);
+    }
+
+    public void setSpeed(double speed) {
+        this.shooterMotor.set(ControlMode.Velocity, speed);
+    }
+
+    public void setPower(double power) {
+        this.shooterMotor.set(ControlMode.PercentOutput, power);
+    }
+    
+    public double getSpeed() {
+        return this.shooterMotor.getSelectedSensorVelocity();
+    }
+
+    @Override
+    public void setupLogging(Logger logger) {
+        logger.addAttribute("Shooter/speed");
+    }
+
+    @Override
+    public void log(Logger logger) {
+        logger.log("Shooter/speed", this.getSpeed());
+    }
+}
