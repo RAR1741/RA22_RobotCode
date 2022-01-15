b11ce13f9ba064e7d3ae86f3779513cc196bdf8e
NotMePipe <mepipeofficial@gmail.com>
2022-01-15 15:26:49 -0500

    Add DriveModule

---
+++ src/main/java/frc/robot/DriveModule.java
@@ -0,0 +1,44 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import frc.robot.logging.Loggable;
+import frc.robot.logging.Logger;
+
+public class DriveModule implements Loggable {
+    
+    private TalonFX main;
+    private TalonFX sub;
+    private String moduleName;
+
+    private double speed;
+
+    DriveModule(String moduleName, int mainID, int subID) {
+        this.moduleName = moduleName;
+        this.main = new TalonFX(mainID);
+        this.sub = new TalonFX(subID);
+    }
+
+    public void setInverted(boolean isInverted) {
+        main.setInverted(isInverted);
+        sub.setInverted(isInverted);
+    }
+
+    public void set(double input) {
+        this.speed = input;
+        main.set(TalonFXControlMode.PercentOutput, input);
+        sub.set(TalonFXControlMode.PercentOutput, input);
+    }
+
+    @Override
+    public void setupLogging(Logger logger) {
+        logger.addAttribute(this.moduleName + "/MotorSpeed");
+        
+    }
+
+    @Override
+    public void log(Logger logger) {
+        logger.log(this.moduleName + "/MotorSpeed", speed);
+    }
+}
