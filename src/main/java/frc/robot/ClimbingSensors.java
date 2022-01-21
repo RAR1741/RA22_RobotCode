b7a0dc9fd2edc8a6b5ee38647c58d73bdf127bb2
selisine <aidenpike113@gmail.com>
2022-01-20 20:17:48 -0500

    Add ClimbingSensors class

---
+++ src/main/java/frc/robot/ClimbingSensors.java
@@ -0,0 +1,32 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.logging.Loggable;
+import frc.robot.logging.Logger;
+
+public class ClimbingSensors implements Loggable {
+    String name;
+    DigitalInput a, b;
+
+    public ClimbingSensors(String name, int aID, int bID) {
+        this.name = name;
+        this.a = new DigitalInput(aID);
+        this.b = new DigitalInput(bID);
+    }
+
+    public boolean get() {
+        return this.a.get() && this.b.get();
+    }
+    
+    @Override
+    public void setupLogging(Logger logger) {
+        logger.log(this.name, this.get());
+        
+    }
+
+    @Override
+    public void log(Logger logger) {
+        // TODO Auto-generated method stub
+        
+    }
+}
