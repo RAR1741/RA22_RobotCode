f9fce03d9bce083c4dd0b52a6fb950c5ab7775ca
NotMePipe <mepipeofficial@gmail.com>
2022-01-22 09:45:58 -0500

    Fix initialization boolean from defaulting null

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,7 +28,7 @@ public class Robot extends TimedRobot {
 
   LoggablePowerDistribution pdp;
 
-  boolean manipulationEnabled;
+  boolean manipulationEnabled = true;
 
   /**
    * This function is run when the robot is first started up and should be used for any
