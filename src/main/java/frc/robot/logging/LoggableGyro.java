dd21d55b6119ed41b0b576c34c8854aad6965cb0
kryllyxofficial01 <kryllyxofficial@gmail.com>
2022-02-21 20:29:33 -0500

    add loggablegyro

---
+++ src/main/java/frc/robot/logging/LoggableGyro.java
@@ -0,0 +1,24 @@
+package frc.robot.logging;
+
+import com.kauailabs.navx.frc.AHRS;
+
+public class LoggableGyro extends AHRS implements Loggable {
+
+    public LoggableGyro() {
+        super();
+    }
+
+    @Override
+    public void setupLogging(Logger logger) {
+        logger.addAttribute("AHRS/velocityY");
+        logger.addAttribute("AHRS/pitch");
+        logger.addAttribute("AHRS/accelerationY");
+    }
+
+    @Override
+    public void log(Logger logger) {
+        logger.log("AHRS/velocityY", this.getVelocityY());
+        logger.log("AHRS/pitch", this.getPitch());
+        logger.log("AHRS/accelerationY", this.getWorldLinearAccelY());
+    }
+}
