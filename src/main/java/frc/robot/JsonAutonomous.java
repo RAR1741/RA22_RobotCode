a850459f98ceb51bc07094ca21f1edfe26008dd9
theblindbandet <me@thisismy.computer>
2022-02-22 19:04:16 -0500

    Fix some of the imports and other assorted errors

--- src/main/java/frc/robot/JsonAutonomous.java
+++ src/main/java/frc/robot/JsonAutonomous.java
@@ -1,45 +1,52 @@
 package frc.robot;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.JsonReader;
+import com.kauailabs.navx.frc.AHRS;
+import frc.robot.logging.LoggableTimer;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
-import com.google.gson.JsonElement;
+public class JsonAutonomous extends Autonomous {
 
-public class JsonAutonomous { //extends Autonomous{
-
-    private final static double TICKS_PER_ROTATION = 1; //TODO: Get values
-    private final static double TICKS_PER_INCH = 1;
+    private static final double TICKS_PER_ROTATION = 1; //TODO: Get values
+    private static final double TICKS_PER_INCH = 1;
     private JsonElement auto;
     private List<AutoInstruction> instructions;
     private int step;
-    private Timer timer;
+    private LoggableTimer timer;
     private double start;
     private double navxStart;
     private AHRS gyro;
+
+    private Drivetrain drive;
 
     private FileReader fr;
     private JsonReader jr;
     private JsonParser jp;
 
-
-    public static class AutoInstruction
-    {
+    public static class AutoInstruction {
         public String type;
         public Unit unit;
         public double amount;
         public List<Double> args;
 
-        
-    
-        public enum Unit { Seconds, Milliseconds, EncoderTicks, Rotations, Inches, Feet, Degrees, Invalid }
+        public enum Unit {
+            SECONDS, MILLISECONDS, ENCODER_TICKS, ROTATIONS, INCHES, FEET, DEGREES, INVALID
+        }
 
-        public AutoInstruction(String type, List<Double> args)
-        {
+        public AutoInstruction(String type, List<Double> args) {
             this.type = type;
             this.args = args;
         }
 
-        public AutoInstruction(String type, Unit unit, double amount, List<Double> args)
-        {
+        public AutoInstruction(String type, Unit unit, double amount, List<Double> args) {
             this.type = type;
             this.unit = unit;
             this.amount = amount;
@@ -51,33 +58,28 @@ public class JsonAutonomous { //extends Autonomous{
      * Creates a JsonAutonomous from the specified file
      * @param file The location of the file to parse
     */
-    public JsonAutonomous(String file, AHRS gyro, SwerveDrive drive) {
-        this.swerve = drive;
+    public JsonAutonomous(String file, AHRS gyro, Drivetrain drive) {
+        this.drive = drive;
         this.gyro = gyro;
 
         parseFile(file);
     }
 
-    public void parseFile(String file)
-    {
+    public void parseFile(String file) {
         step = -1;
-        timer = new Timer();
+        timer = new LoggableTimer();
         instructions = new ArrayList<AutoInstruction>();
-        try
-        {
+        try {
             fr = new FileReader(file);
             jr = new JsonReader(fr);
             auto = JsonParser.parseReader(jr);
             JsonElement inner = auto.getAsJsonObject().get("auto");
-            if(inner.isJsonArray())
-            {
-                for(JsonElement e : inner.getAsJsonArray())
-                {
+            if (inner.isJsonArray()) {
+                for (JsonElement e : inner.getAsJsonArray()) {
                     JsonObject o = e.getAsJsonObject();
 
                     List<Double> extraArgs = new ArrayList<Double>();
-                    for(JsonElement e2 : o.get("args").getAsJsonArray())
-                    {
+                    for (JsonElement e2 : o.get("args").getAsJsonArray()) {
                         extraArgs.add(e2.getAsDouble());
                     }
 
@@ -88,31 +90,27 @@ public class JsonAutonomous { //extends Autonomous{
 
                     double amount = o.has("amount") ? o.get("amount").getAsDouble() : null;
 
-                    AutoInstruction ai = unit == null ? new AutoInstruction(type, extraArgs) : new AutoInstruction(type, unit, amount, extraArgs);
+                    AutoInstruction ai = unit == null ? new AutoInstruction(type, extraArgs)
+                            : new AutoInstruction(type, unit, amount, extraArgs);
                     instructions.add(ai);
                 }
             }
-        }
-        catch (JsonIOException | JsonSyntaxException | FileNotFoundException e)
-        {
+        } catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public static AutoInstruction.Unit parseUnit(String in)
-    {
+    public static AutoInstruction.Unit parseUnit(String in) {
         return AutoInstruction.Unit.valueOf(in);
     }
 
     @Override
-    public void run(){
-        if(step == -1)
-        {
+    public void run() {
+        if (step == -1) {
             reset();
         }
-        if(instructions.size() == step)
-        {
-            swerve.swerve(0, 0, 0, getAngle()-navxStart, false);
+        if (instructions.size() == step) {
+            drive.swerve(0, 0, 0, getAngle() - navxStart, false);
             return;
         }
         AutoInstruction ai = instructions.get(step);
@@ -144,5 +142,5 @@ public class JsonAutonomous { //extends Autonomous{
                 break;
         }
     }
-    
+
 }
