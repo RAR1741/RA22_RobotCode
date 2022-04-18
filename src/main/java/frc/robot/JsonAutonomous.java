05dc2000b0c0d08103a2322ed796d996edf40fe8
GingerJake <uniquereddy90@gmail.com>
2022-04-18 17:28:21 -0400

    Update ticks per rotation value

--- src/main/java/frc/robot/JsonAutonomous.java
+++ src/main/java/frc/robot/JsonAutonomous.java
@@ -21,7 +21,7 @@ import java.util.NoSuchElementException;
 
 public class JsonAutonomous extends Autonomous implements Loggable {
 
-    private static final double TICKS_PER_ROTATION = 16750; // TODO: Update value for 2022 robot
+    private static final double TICKS_PER_ROTATION = 47102; // TODO: Update value for 2022 robot
     private static final double TICKS_PER_INCH = TICKS_PER_ROTATION / (6 * Math.PI); // TODO: Update
                                                                                      // formula for
                                                                                      // 2022 robot
