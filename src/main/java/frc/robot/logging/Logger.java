2918f294eea7f4fd46f802960a8268fac3a53559
Kryllyx <kryllyxofficial@gmail.com>
2022-06-30 20:55:59 -0400

    fix month index

--- src/main/java/frc/robot/logging/Logger.java
+++ src/main/java/frc/robot/logging/Logger.java
@@ -32,7 +32,7 @@ public class Logger {
 		Calendar calendar = Calendar.getInstance();
         String dir = "\\home\\lvuser\\logs";
 		Path path = Paths.get(dir + "\\" + calendar.get(Calendar.YEAR) + "-"
-		+ calendar.get(Calendar.MONTH) + "-" + calendar.get(Calendar.DAY_OF_MONTH) + "-"
+		+ (calendar.get(Calendar.MONTH) + 1) + "-" + calendar.get(Calendar.DAY_OF_MONTH) + "-"
 		+ calendar.get(Calendar.HOUR_OF_DAY) + "-" + calendar.get(Calendar.MINUTE) + "-"
 		+ calendar.get(Calendar.SECOND) + ".csv");
 		
