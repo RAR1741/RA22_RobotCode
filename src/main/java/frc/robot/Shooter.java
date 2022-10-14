ac37ac547e18c1abcfb528ff26756b583a31fa07
GingerJake <uniquereddy90@gmail.com>
2022-10-13 20:52:12 -0400

    add more motors to Shooter class

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -4,24 +4,30 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import frc.robot.logging.Loggable;
 import frc.robot.logging.Logger;
 
 public class Shooter implements Loggable {
     TalonFX shooterMotor;
+    CANSparkMax indexWheel;
+    CANSparkMax stopWheel;
 
-    public Shooter(TalonFX s) {
-        this.shooterMotor = s;
+    public Shooter(TalonFX shooter, CANSparkMax index, CANSparkMax stop) {
+        this.shooterMotor = shooter;
+        this.indexWheel = index;
+        this.stopWheel = stop;
 
         this.shooterMotor.setNeutralMode(NeutralMode.Coast);
     }
 
     public void setSpeed(double speed) {
-        this.shooterMotor.set(ControlMode.Velocity, speed);
+            this.shooterMotor.set(ControlMode.Velocity, speed);  
+        }
     }
 
-    public void setPower(double power) {
+    public void setPower(double power, String motor) {
         this.shooterMotor.set(ControlMode.PercentOutput, power);
     }
 
